c143822c9d8c31d08ae358b6d446e8d32157f5ec
jnani.weibel <jnani.weibel@visioncritical.com>
2018-01-14 22:07:48 -0800

    Add vectors.s

---
+++ src/asm/vectors.s
@@ -0,0 +1,14 @@
+
+.global interrupt_handler_swi
+interrupt_handler_swi:
+  push {r0-r12, lr} // TODO: This should be implicit by ARM interrupt... (possible stack problem)
+  bl interrupt_swi
+  pop {r0-r12, lr}
+  eret
+
+.global interrupt_handler_irq
+interrupt_handler_irq:
+  push {r0-r12, lr}
+  bl interrupt_irq
+  pop {r0-r12, lr}
+  eret
