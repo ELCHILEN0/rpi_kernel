a72d351011037649754c9d6b5786016a9a9fa1ac
jnani.weibel <jnani.weibel@visioncritical.com>
2018-01-18 00:30:32 -0800

    Fix return semantics

--- src/asm/vectors.s
+++ src/asm/vectors.s
@@ -1,4 +1,3 @@
-
 .global interrupt_handler_swi
 .global interrupt_handler_undef
 .global interrupt_handler_pftch
@@ -6,14 +5,20 @@
 .global interrupt_handler_fiq
 .global interrupt_handler_irq
 
+/**
+ * Exception handling semantics come from ch. 11-12 in ARM Cortex-A Manual.  Strictly
+ * speaking, interrupt sources should be cleared before the handler is invoked,
+ * this will be important in cases with reentrant interrupts.  For nested interrupts
+ * see ch. 12.1.3.
+ */
+
 interrupt_handler_swi:
   push {r0-r12, lr} // TODO: This should be implicit by ARM interrupt... (possible stack problem)
   bl interrupt_swi
   pop {r0-r12, lr}
-  //cpsie aif eret handles this....
-  eret
+  MOVS PC, lr
 
-interrupt_handler_fiq:
+interrupt_handler_fiq: // TODO: Different handler...
 interrupt_handler_irq:
   push {r0-r12, lr}
   bl interrupt_irq
@@ -26,7 +31,6 @@ interrupt_handler_abt:
   pop {r0-r12, lr}
   SUBS PC, lr, #8
 
-
 interrupt_handler_pftch:
   push {r0-r12, lr}
   bl interrupt_irq_other
@@ -38,13 +42,3 @@ interrupt_handler_undef:
   bl interrupt_irq_other
   pop {r0-r12, lr}
   MOVS PC, lr
-
-/* ARM 11.3
-
-MOVS PC, R14 
-MOVS PC, R14 
-SUBS PC, R14, #4 
-SUBS PC, R14, #8 
-SUBS PC, R14, #4 
-SUBS PC, R14, #4
-*/
