ddead22e85da9eb69ca33158aa9b6d5efcff66b9
jnani.weibel <jnani.weibel@visioncritical.com>
2018-03-05 01:02:36 -0800

    Fix perf

--- src/c/init.c
+++ src/c/init.c
@@ -75,9 +75,9 @@ void cinit_core(void) {
             // TODO: BuildID
             pmu_enable();
             pmu_config_pmn(0, 0x8);
-            pmu_config_pmn(1, 0x13);
-            pmu_config_pmn(2, 0x66);
-            pmu_config_pmn(3, 0x67);
+            pmu_config_pmn(1, 0x11);
+            pmu_config_pmn(2, 0x13);
+            pmu_config_pmn(3, 0x19);
 
             pmu_enable_pmn(0);
             pmu_enable_pmn(1);
@@ -88,9 +88,7 @@ void cinit_core(void) {
 
             // pmu_reset_ccnt();
             // pmu_reset_pmn();
-            for (int i = 0; i < 100; i++) {
-                asm volatile("nop");
-            }
+
             uint64_t perf[5] = {
                     pmu_read_ccnt(),
                     pmu_read_pmn(0),
@@ -99,9 +97,11 @@ void cinit_core(void) {
                     pmu_read_pmn(3),
                 };
 
-            while(true);
-            // printf("perf: %d, %d\r\n", perf[0], perf[1]);
-
+            printf("counter: %lud\r\n", perf[0]);
+            printf("instrs: %lud\r\n", perf[1]);
+            printf("cycles: %lud\r\n", perf[2]);
+            printf("mem access: %lud\r\n", perf[3]);
+            printf("bus access: %lud\r\n", perf[4]);
 
             // timer_frequency = (2e31/prescaler) * input_frequency
             // APB vs CRY ...   1/2 CPU Frequency vs Fixed Time Frequency
--- src/c/perf.c
+++ src/c/perf.c
@@ -1,6 +1,7 @@
 #include "perf.h"
 
 // https://developer.arm.com/products/software-development-tools/ds-5-development-studio/resources/tutorials/using-the-pmu-event-counters-in-ds-5
+// https://developer.arm.com/docs/ddi0500/latest/performance-monitor-unit/events
 
 void pmu_enable() {
     uint64_t reg;
@@ -27,23 +28,17 @@ void pmu_reset_ccnt() {
 }
 
 uint64_t pmu_read_ccnt() {
-    // uint64_t reg;
-    // asm("MRS %0, PMCCNTR_EL0" :: "=r" (reg));
-    // return reg;
     uint64_t reg;
     asm volatile("MRS %0, PMCCNTR_EL0" : "=r" (reg));
     return reg;
 }
 
-//  MSR PSR, RD ; PSR <= RD
-//  MRS RD, PSR ; R1 <= PSR
 void pmu_enable_ccnt() {
     uint64_t reg = 0x80000000;
     asm("MSR PMCNTENSET_EL0, %0" :: "r" (reg));
 }
 
 uint64_t pmu_read_pmn(uint8_t counter) {
-    // counter &= 0x1F;
     uint64_t reg = counter & 0x1F;
     asm("MSR PMSELR_EL0, %0" :: "r" (reg));
     asm("ISB");
@@ -56,15 +51,15 @@ void pmu_config_pmn(uint8_t counter, uint32_t event) {
     uint64_t reg = counter & 0x1F;
     asm("MSR PMSELR_EL0, %0" :: "r" (reg));
     asm("ISB");
-    asm("MSR PMXEVTYPER_EL0, %0" : "=r" (event));
+    asm("MSR PMXEVTYPER_EL0, %0" :: "r" (event));
 }
 
 void pmu_enable_pmn(uint8_t counter) {
     uint64_t reg = 1 << counter;
-    asm("MSR PMCNTENSET_EL0, %0" : "=r" (reg));
+    asm("MSR PMCNTENSET_EL0, %0" :: "r" (reg));
 }
 
 void pmu_disable_pmn(uint8_t counter) {
     uint64_t reg = 1 << counter;
-    asm("MSR PMCNTENCLR_EL0, %0" : "=r" (reg));
+    asm("MSR PMCNTENCLR_EL0, %0" :: "r" (reg));
 }
